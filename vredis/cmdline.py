1d7748d3ad7c3a48323fd1c83739e843dd040b17
wb-zhoulin1 <wb-zhoulin1@alibaba-inc.com>
2018-12-21 15:41:25 +0800

    add

--- vredis/cmdline.py
+++ vredis/cmdline.py
@@ -1,3 +1,55 @@
-def execute(argv=None, settings=None):
-    # 尚在开发当中。
-    pass
+import sys
+import argparse
+
+VREDIS_COMMAND_TYPES = ['list','run','attach','script'] 
+
+description = '''
+vredis <command> [options] [args]
+
+command
+------------------------------------------
+  list      show info.
+  run       run.
+  cmdline   use cmdline connect host.
+  script    script.
+  attach    attach.
+
+  <command> -h|--help   ::show subcommand info
+
+defaults
+------------------------------------------
+  --host    redis host. default: localhost
+  --host    redis port. default: 6379
+
+'''
+
+epilog = '''
+
+epilog
+------------------------------------------
+  .
+
+'''
+
+def execute():
+
+    # 处理命令行
+    parse = argparse.ArgumentParser(
+        usage           = None,
+        epilog          = epilog,
+        formatter_class = argparse.RawDescriptionHelpFormatter,
+        description     = description,
+        add_help        = False)
+    parse.add_argument('command',choices=VREDIS_COMMAND_TYPES,help=argparse.SUPPRESS)
+    parse.add_argument('--host',default='localhost',help=argparse.SUPPRESS)
+    parse.add_argument('--port',default='6379',help=argparse.SUPPRESS)
+    parse.add_argument('--filter',default=None,help=argparse.SUPPRESS)
+
+    if   len(sys.argv) == 1 or \
+        (len(sys.argv) == 2 and \
+             sys.argv[1] in ['-h','--help']):
+        parse.print_help()
+        sys.exit(0)
+
+    args = parse.parse_args()
+    
